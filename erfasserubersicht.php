98827c5d728fd2fabfee886fcb3c3994a6b08e07
huber <huber@MPR31332.ab.ba.ba-ravensburg.de>
2015-07-14 12:55:52 +0200

    fragebogenubersicht bearbeitet

---
+++ erfasserubersicht.php
@@ -0,0 +1,30 @@
+<!-- Fragebogenübersicht -->
+<table border="1">
+  <tr>
+    <th>Fragebogentitel</th>
+    <th>bearbeiten</th>
+    <th>kopieren</th>
+    <th>auswerten</th>
+    
+  </tr>
+  
+<!-- Loop über Fragebögen -->
+  <tr style="valign: middle;">
+    <td>Testtitel</td>
+    <td>
+    	<form action="fragebogenbearbeiten.php" method="post">
+    	<input type="hidden" name="fragebogentitel" value="Testtitel">    
+    	<input type="submit" value="bearbeiten">
+    	</form>
+    </td>
+  </tr>
+<!-- Ende Loop -->  
+  
+</table>
+
+
+
+<form action="neuerfragebogen.php" method="post">
+<input type="submit" value="Neuer Fragebogen">
+
+</form>
