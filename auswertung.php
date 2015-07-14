1d29e0260aa427345fe03a771bb802ab8599ad28
huber <huber@MPR31324.ab.ba.ba-ravensburg.de>
2015-07-14 11:06:43 +0200

    Formulartest und Auswertung

---
+++ auswertung.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Auswertung</h1>";
+echo $_POST['text']."<br>";
+echo $_POST['text2']."<br>";
+?>
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-echo "test";
-echo "test2";
-?>
+<html>
+<head>
+</head>
+<body>
+<form action="auswertung.php" method="post">
+<input type="text" name="text"><br>
+<input type="text" name="text2"><br>
+<input type="submit" name="absenden" ="Absenden">
+
+
+</form>
+</body>
+</html>
