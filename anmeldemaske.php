b6d92416455ebac14f106905ae36a70a4db70d89
Stark.J <Stark.J@MPR31334.ab.ba.ba-ravensburg.de>
2015-07-28 11:12:16 +0200

    Fragebogenuebersicht, uberarbeitete anmeldemaske und mnrnichtvorhanden (Seite für Fehlschlag bei Anmeldung Student)

--- anmeldemaske.php
+++ anmeldemaske.php
@@ -1,3 +1,7 @@
+<?php 
+include 'datenbankconnect.php';
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,11 +14,28 @@
 	
 	<h2>Bitte Anmelden via Matrikelnummer</h2>
 	<form action="fragebogenuebersicht.php" method="post">
+	
+
+	
+	
+	
+	<!-- Hier fehlt Gegenprüfung ob MNR vorhanden -> JA-> fragebogenuebersicht.php ; NEIN -> mnrnichtvorhanden.php -->
+	
 		Deine Matrikelnummer <input name="mnr_student" type="text" size="40" maxlength="30" > <br>
 		<input type="submit" name="anmeldung_mnr" = "anmelden"> <br>
 		
 		
+	<?php 
+	$result = mysqli_query($con, "SELECT mnr from student");
+		
 	
+	while ($row = mysqli_fetch_assoc($result))	
+	{
+		print ($row ["mnr"]);
+	}
+	mysqli_close($con);
+	
+	?>
 	
 	
 	<h3>Die alternative Anmeldung fÃ¼r Erfasser ist unter folgendem Link zu finden</h3>
---
+++ mnrnichtvorhanden.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<title>Fehler</title>
+		
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>Diese Matrikelnummer ist nicht hinterlegt!</h1>
+			</header>
+			<div>
+				<h2>Bitte Eingaben checken</h2>
+				<a href=anmeldemaske.php>Zur Anmeldemaske</a> 
+			</div>
+		</div>
+	</body>
+</html>
