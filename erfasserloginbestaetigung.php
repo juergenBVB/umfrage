5dd2138a6e237d5df11837b563bd61847f095f09
Aierstock <Aierstock@MPR31332.ab.ba.ba-ravensburg.de>
2015-08-17 16:29:14 +0200

    Erfasseranmeldung vollständig mit Check bei Registrierung, ob Benutzer schon vorhanden

--- erfasserloginbestaetigung.php
+++ erfasserloginbestaetigung.php
@@ -1,52 +1,64 @@
-/* checken ob beide Felder befüllt, ob Benutzername belegt, kennwortlänge lang genug,
- aber nicht zu lange */
-
 <?php session_start(); 
-include 'datenbankconnect.php';?>
+include 'datenbankconnect.php';
+
+/* Könnte man bei Login/Registrierung noch checken: Beide Felder befüllt, kennwortlänge lang genug,
+ aber nicht zu lange  */
+?>
 
 <html>
 	<head>
 	</head>
 	<body>	
-		<h1> Anmeldung erfolgreich!	</h1>		
+	
 		<?php
 		$benutzername = $_POST['ebenutzername'];
 		$kennwort = $_POST['ekennwort'];
-	/*	echo 'Benutzername: '.$benutzername;
-		echo '<br>Kennwort: '.$kennwort;		*/
-		
 			
 		if ($_POST['buttons'] == "Einloggen")
 		{
-
 			$sqlresult =
 				mysqli_query($con, "Select kennwort From erfasser where benutzername='$benutzername';");
 			
 			while ($row = mysqli_fetch_assoc($sqlresult))
 			{
 				if ($row['kennwort']==$kennwort){
-					echo 'Benutzeranmeldung erfolgreich.
+					echo '<h1> Benutzeranmeldung erfolgreich. </h1>
 					<a href=erfasserubersicht.php> <br> <br> <h3>Zur Erfasserübersicht</h3></a>';
 					$_SESSION ["benutzername"]=$benutzername;
 				}
 				else
 				{
-					echo 'Kennwort oder Benutzername falsch. <br>
+					echo '<h1> Anmeldung fehlgeschlagen </h1>
+						Kennwort oder Benutzername falsch. <br>
 						Bitte versuchen Sie es noch einmal.<br>
-						<a href=erfasserubersicht.php> <br> <br> <h3>Zur Erfasserübersicht</h3></a>';
+						<a href=erfasserlogin.php> <br> <br> <h3>Zurück zum Erfasserlogin</h3></a>';
 				}
 			}
 		}
 		
 		if ($_POST['buttons'] == "Registrieren")
 		{
-			mysqli_query($con, "insert into erfasser values ('$benutzername','$kennwort')");
+			$sqlresult =
+				mysqli_query($con, "Select benutzername from erfasser where benutzername='$benutzername';");
 			
-			echo 'Benutzer <b>'.$benutzername.'</b> wurde gespeichert. <br>
-				Sie können sich	nun auf der Startseite einloggen. 
-				<a href=erfasserlogin.php> <br> <br> <h3>Zur Startseite</h3></a>' ;
-		}
+			
+				if (mysqli_num_rows($sqlresult)==1){
+					echo '<h1>Registrierung fehlgeschlagen</h1> <br> Benutzername bereits vergeben
+				<a href=erfasserlogin.php> <br> <br> <h3>Zurück zur Registrierung</h3></a>';
+				
+				}
+				else {	
+					mysqli_query($con, "insert into erfasser values ('$benutzername','$kennwort')");
+						
+					echo '<h1> Registrierung erfolgreich </h1>
+							<br> Benutzer <b>'.$benutzername.'</b> wurde gespeichert. <br>
+				Sie können sich	nun auf der Startseite einloggen.
+				<a href=erfasserubersicht.php> <br> <br> <h3>Zur Startseite</h3></a>' ;
+				}
+			}		
+
 		
+	
 		?>
 	</body>
 	
