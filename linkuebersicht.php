3e6e2d40f047dc540462ec141d6b3574aabfed91
Stark.J <Stark.J@MPR31334.ab.ba.ba-ravensburg.de>
2015-08-17 17:04:09 +0200

    Linkübersicht aktueller Stand

--- linkuebersicht.php
+++ linkuebersicht.php
@@ -7,33 +7,30 @@
 <title>Linkübersicht</title>
 </head>
 <body>
-		<h1> Hier finden Sie eine Uebersicht über alle Links / direkter Zugriff auf die Seite</h1>
+		<h1> Hier finden Sie eine Uebersicht aller Links / direkter Zugriff auf die Seite</h1>
 	
 	 <br>
-	 
-	 
-	 
-	 
 
 	
-	 <a href=anmeldemaske.php>Anmeldemaske</a>
+	 <a href=anmeldemaske.php>Anmeldemaske</a> <br>
+	 	 
+	 <a href=datenbankconnect.php>Datenbankconnect</a><br>
 	 
+	 <a href=erfasserlogin.php> Erfasserlogin</a><br>
 	 
-	 <a href=datenbankconnect.php>Datenbankconnect</a>
+	 <a href=erfasserloginbestaetigung> Erfasserloginbestaetigung</a><br>
 	 
-	 <a href=erfasserlogin.php> Erfasserlogin</a>
+	 <a href=fragebogenuebersicht.php> Fragebogenuebersicht</a><br>
 	 
-	 <a href=erfasserloginbestaetigung> Erfasserloginbestaetigung</a>
+	 <a href=gegencheckmnranmeldung.php> Gegencheckmnranmeldung</a><br>
 	 
-	 <a href=fragebogenuebersicht.php> Fragebogenuebersicht</a>
+	 <a href=index.php>Index</a><br>
 	 
-	 <a href=gegencheckmnranmeldung.php> Gegencheckmnranmeldung</a>
+	 <a href=mnrnichtvorhanden.php>Matrikelnummer nicht vorhanden</a><br>
 	 
-	 <a href=index.php>Index</a>
+	 <a href= neuerfragebogen.php> Neuer Fragebogen</a><br>
 	 
-	 <a href=mnrnichtvorhanden.php>Matrikelnummer nicht vorhanden</a>
-	 
-	 <a href= neuerfragebogen.php> Neuer Fragebogen</a>
+	 <a href="></a>
 	
 	
 
